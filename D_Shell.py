54297a04b323e2551fb5cd7c0060a807bcb8a255
dor <c13469208@mydit.ie>
2016-10-23 17:38:27 +0100

    Final Push

---
+++ D_Shell.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+"""Usage:  tsh [options] [file]
+
+"""
+
+
+import sys, os, os.path, glob, re
+import datetime
+import time
+from pwd import getpwnam
+import grp
+import getpass
+from os.path import expanduser
+
+# The variables can be changed by the command line options:
+VERBOSE = False
+
+# Location of main configuration file:
+CONFIG = '/bin/D_Shell/D_Shell.conf'
+
+allowExec = []
+allowSubShell = False
+
+def pwd():
+    home = expanduser("~")
+    homeid = os.stat(home)
+    gid = os.getgid()
+    group_info = grp.getgrgid(gid)
+    username = getpass.getuser()
+    print 'UserID:',getpwnam(username).pw_uid
+    print 'GroupID:',gid
+    print 'GroupName:',group_info.gr_name
+    print 'UserName :',getpwnam(username).pw_gecos
+    print 'Inode:',homeid.st_ino
+
+def date():
+
+    thedatetime = time.strftime('%Y%m%d%H%M%S')
+    print 'DateTime:',thedatetime
+
+def readConfig():
+    if os.access(CONFIG, os.R_OK):
+        exec file(CONFIG) in globals()
+    else:
+        print "D-SHELL: Warning: Could not open `%s' for reading." % CONFIG
+
+def lineSplit(line):
+    line = line.strip()
+    if not line or line[0]=='#':
+        return '', ''
+    tmp = line.split(None, 1)
+    if tmp:
+        return tmp[0], '' if len(tmp)==1 else tmp[1].strip()
+
+
+def lock_down(cmd):
+    allowed_arr = ['pw', 'dt', 'ud', 'ifc','exit','help']
+    if cmd not in allowed_arr:
+        return "'" + cmd +"'"
+    else:
+        return cmd
+def help ():
+    with open('/etc/issue.net', 'r') as fin:
+        print fin.read()
+
+
+class Shell:
+    def __init__(self):
+        self.alias = {}
+
+    def do_alias(self, args):
+        m = re.match(r"((\S+)='([^']+)')?$", args)
+        if m:
+            if m.group(2):
+                self.alias[m.group(2)] = m.group(3)
+            else:
+                for kv in self.alias.iteritems():
+                    print "alias %s='%s'" % kv
+        else:
+            print "D-SHELL: Usage: alias [name='value']"
+
+
+    def execute_file(self, filename):
+        if VERBOSE: print "D-SHELL: executing file `%s':" % filename
+        for line in file(filename):
+            self.execute(line)
+
+    def execute(self, line):
+        line = line.strip()
+        cmd, args = lineSplit(line)
+
+       # here I process the command to remove any args and then place a default in for incon -DOR
+        if (cmd == 'pw'):
+            args = ''
+        if (cmd == 'ifc' and args == ''):
+            args = 'eth0'
+        if (cmd == 'dt'):
+            date()
+            return
+        if (cmd == 'ud'):
+            pwd()
+            cmd = ""
+            return
+        if (cmd == 'help'):
+            help()
+
+        if not cmd:
+            return
+        f = self.rawExec
+        if self.alias.has_key(cmd):
+            alias = self.alias[cmd]
+            line = alias + ' ' + args
+            if lineSplit(alias)[0] != cmd:
+                f = self.execute
+        f(line)
+
+    def rawExec(self, line):
+        if VERBOSE: print "D-SHELL: executing: %s   " % line,
+        cmd, args = lineSplit(line)
+        funcname = "do_" + cmd
+        if hasattr(self, funcname):
+            if VERBOSE: print "(shell builtin)"
+            func = getattr(self, funcname)
+            try:
+                func(args)
+            except:
+                print "D-SHELL: Error in line `%s'." % line
+        elif cmd in allowExec:
+            if VERBOSE: print "(os.spawn)"
+            os.spawnvp(os.P_WAIT, cmd, line.split())
+        else:
+            if VERBOSE: print "(on subshell)"
+            if allowSubShell:
+                os.system(line)
+            else:
+                print "D-SHELL: `%s' Permission denied." % line
+
+    def commands(self):
+        """
+        >>> s = Shell()
+        >>> s.commands()['cd']
+        'builtin'
+        """
+        res = {'exit': 'builtin'}
+        for var in vars(self.__class__):
+            if var.startswith('do_'):
+                res[var[3:]] = 'builtin'
+        for c in allowExec:
+            res[c] = 'OS'
+        for k, v in self.alias.items():
+            tmp = "aliased '%s'" % v
+            if res.has_key(k):
+                res[k] += ", " + tmp
+            else:
+                res[k] = tmp
+        return res
+
+    def completions(self):
+        res = self.commands()
+        for f in glob.glob('*'):
+            res[f] = True
+        return res
+
+
+def repl():
+    try:
+        import readline
+        has_readline = True
+    except ImportError:
+        has_readline = False
+
+    shell = Shell()
+
+    rcFile = os.path.expanduser('/bin/D_Shell/D_Shellrc')
+    if os.access(rcFile, os.R_OK):
+        shell.execute_file(rcFile)
+
+    while True:
+        words = shell.completions()
+        try:
+            line = raw_input(' D-SHELL> ')
+            cmd, args = lineSplit(line)
+            line = lock_down(cmd) + ' ' + args
+            if line.strip() == 'exit':
+                break
+        except EOFError:
+            print 'exit'
+            break
+        shell.execute(line)
+
+
+def usage():
+    print __doc__
+    sys.exit(0)
+
+
+
+if __name__ == '__main__':
+    import getopt
+    readConfig()
+    repl()
